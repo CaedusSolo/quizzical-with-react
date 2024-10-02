56c04f05016d6b1985871b18b2a9408b234b520c
Ying Tong <yingtong0106@gmail.com>
2024-10-02 12:31:05 +0800

    Installed he package to decode HTML entities

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import './App.css'
-import StartPage from './components/StartPage'
+import "./App.css";
+import StartPage from "./components/StartPage";
+import React, { useState, createContext } from "react";
+import QuizPage from "./components/QuizPage";
+import he from "he"
+
+export const AppContext = createContext();
+
 
 function App() {
+  const [quizStarted, setQuizStarted] = useState(false);
+
+
+  function onStartBtnClick() {
+    setQuizStarted((prevState) => !prevState);
+  }
+
+
 
   return (
     <>
-      <StartPage />
+      <AppContext.Provider
+        value={{
+          onStartBtnClick,
+        }}
+      >
+        <QuizPage />
+      </AppContext.Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../App'
 
 function StartPage() {
+
+  const {onStartBtnClick} = useContext(AppContext)
+
   return (
     <div className='startPage'>
         <h3>Quizzical</h3>
-        <button className="btn">Begin Quiz</button>
+        <button className="btn" onClick={onStartBtnClick}>Begin Quiz</button>
     </div>
   )
 }
